bfb544b111d55c88d0ad807b02b528cec243dec8
Rohit Sahu <113296241+Rohit-304@users.noreply.github.com>
2023-06-08 19:20:50 +0530

    Delete a Node

---
+++ DSA/LinkedList/Delete_at_SpecificPosition.java
@@ -0,0 +1,236 @@
+/*
+This challenge is part of a tutorial track by MyCodeSchool and is accompanied by a video lesson.
+
+Delete the node at a given position in a linked list and return a reference to the head node. The head is at position 0. The list may be empty after you delete the node. In that case, return a null value.
+
+Example
+
+
+
+After removing the node at position , .
+
+Function Description
+
+Complete the deleteNode function in the editor below.
+
+deleteNode has the following parameters:
+- SinglyLinkedListNode pointer llist: a reference to the head node in the list
+- int position: the position of the node to remove
+
+Returns
+- SinglyLinkedListNode pointer: a reference to the head of the modified list
+
+Input Format
+
+The first line of input contains an integer , the number of elements in the linked list.
+Each of the next  lines contains an integer, the node data values in order.
+The last line contains an integer, , the position of the node to delete.
+
+Constraints
+
+, where  is the  element of the linked list.
+Sample Input
+
+8
+20
+6
+2
+19
+7
+4
+15
+9
+3
+Sample Output
+
+20 6 2 7 4 15 9
+Explanation
+
+The original list is . After deleting the node at position , the list is 
+*/
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class SinglyLinkedListNode {
+    public int data;
+    public SinglyLinkedListNode next;
+
+    public SinglyLinkedListNode(int nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    public SinglyLinkedListNode head;
+    public SinglyLinkedListNode tail;
+
+    public SinglyLinkedList() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void insertNode(int nodeData) {
+        SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+        }
+
+        this.tail = node;
+    }
+}
+
+class SinglyLinkedListPrintHelper {
+    public static void printList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+}
+
+class Result {
+
+    /*
+     * Complete the 'deleteNode' function below.
+     *
+     * The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+     * The function accepts following parameters:
+     *  1. INTEGER_SINGLY_LINKED_LIST llist
+     *  2. INTEGER position
+     */
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+
+    public static SinglyLinkedListNode deleteNode(SinglyLinkedListNode llist, int position) {
+    // Write your code here
+    if(llist == null){
+        return null;
+    }
+    if(position == 0){
+        return llist.next;
+    }
+    SinglyLinkedListNode tempnode = llist;
+    for(int i = 1; i < position; i++){
+        tempnode=tempnode.next;
+    }
+    if(tempnode.next != null){
+        tempnode.next = tempnode.next.next;
+    }
+    else{
+        tempnode.next = null;
+    }
+    return llist;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        SinglyLinkedList llist = new SinglyLinkedList();
+
+        int llistCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, llistCount).forEach(i -> {
+            try {
+                int llistItem = Integer.parseInt(bufferedReader.readLine().trim());
+
+                llist.insertNode(llistItem);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int position = Integer.parseInt(bufferedReader.readLine().trim());
+
+        SinglyLinkedListNode llist1 = Result.deleteNode(llist.head, position);
+
+        SinglyLinkedListPrintHelper.printList(llist1, " ", bufferedWriter);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
+
+/*
+Test case 1:
+Input (stdin)
+8
+20
+6
+2
+19
+7
+4
+15
+9
+3
+Your Output (stdout)
+20 6 2 7 4 15 9
+Expected Output
+20 6 2 7 4 15 9
+
+result:
+Success
+
+Test case 2:
+Input (stdin)
+4
+11
+9
+2
+9
+1
+Your Output (stdout)
+11 2 9
+Expected Output
+11 2 9
+
+result:
+Success
+
+*/
+   
+    
+  
+  
+    
+    
+    
+    
+    
+    
+    
+    
