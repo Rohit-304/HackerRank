71664273554296e10139d6c249502b6efdc727bb
Rohit Sahu <113296241+Rohit-304@users.noreply.github.com>
2023-06-17 09:10:07 +0530

    Insert a node at the head of a linked list

---
+++ DSA/LinkedList/Insert_at_head.java
@@ -0,0 +1,184 @@
+/*
+This challenge is part of a tutorial track by MyCodeSchool and is accompanied by a video lesson.
+
+Given a pointer to the head of a linked list, insert a new node before the head. The  value in the new node should point to  and the  value should be replaced with a given value. Return a reference to the new head of the list. The head pointer given may be null meaning that the initial list is empty.
+
+Function Description
+
+Complete the function insertNodeAtHead in the editor below.
+
+insertNodeAtHead has the following parameter(s):
+
+SinglyLinkedListNode llist: a reference to the head of a list
+data: the value to insert in the  field of the new node
+Input Format
+
+The first line contains an integer , the number of elements to be inserted at the head of the list.
+The next  lines contain an integer each, the elements to be inserted, one per function call.
+
+Constraints
+
+Sample Input
+
+5
+383
+484
+392
+975
+321
+Sample Output
+
+321
+975
+392
+484
+383
+Explanation
+
+Intially the list in NULL. After inserting 383, the list is 383 -> NULL.
+After inserting 484, the list is 484 -> 383 -> NULL.
+After inserting 392, the list is 392 -> 484 -> 383 -> NULL.
+After inserting 975, the list is 975 -> 392 -> 484 -> 383 -> NULL.
+After inserting 321, the list is 321 -> 975 -> 392 -> 484 -> 383 -> NULL.
+*/
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+      
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the insertNodeAtHead function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static SinglyLinkedListNode insertNodeAtHead(SinglyLinkedListNode llist, int data) {
+        SinglyLinkedListNode node = new SinglyLinkedListNode(data);
+        if(llist == null){
+            return node;
+        }
+        node.next = llist;
+        return node;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        SinglyLinkedList llist = new SinglyLinkedList();
+
+        int llistCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < llistCount; i++) {
+            int llistItem = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        	
+          SinglyLinkedListNode llist_head = insertNodeAtHead(llist.head, llistItem);
+
+          llist.head = llist_head;
+        }
+
+        printSinglyLinkedList(llist.head, "\n", bufferedWriter);
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+
+/*
+Test Case 1:
+Compiler Message
+Success
+Input (stdin)
+5
+383
+484
+392
+975
+321
+Expected Output
+321
+975
+392
+484
+383
+
+Test Case 2:
+Compiler Message
+Success
+Input (stdin)
+10
+321
+641
+653
+524
+952
+337
+955
+891
+590
+133
+Expected Output
+133
+590
+891
+955
+337
+952
+524
+653
+641
+321
+
+*/
+
