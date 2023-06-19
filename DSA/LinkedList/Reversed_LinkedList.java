ac6d81d226d40503473132d66873402a6994d88b
Rohit Sahu <113296241+Rohit-304@users.noreply.github.com>
2023-06-19 08:20:38 +0530

    Reverse a linked list

--- DSA/LinkedList/Reversed_LinkedList.java
+++ DSA/LinkedList/Reversed_LinkedList.java
@@ -18,8 +18,8 @@ SinglyLinkedListNode pointer head: a reference to the head of a list
 Returns
 
 SinglyLinkedListNode pointer: a reference to the head of the reversed list
-Input Format
 
+Input Format
 The first line contains an integer , the number of test cases.
 
 Each test case has the following format:
@@ -27,11 +27,9 @@ Each test case has the following format:
 The first line contains an integer , the number of elements in the linked list.
 Each of the next  lines contains an integer, the  values of the elements in the linked list.
 
-Constraints
+Constraints, where  is the  element in the list.
 
-, where  is the  element in the list.
 Sample Input
-
 1
 5
 1
@@ -39,11 +37,11 @@ Sample Input
 3
 4
 5
+
 Sample Output
-
 5 4 3 2 1 
-Explanation
 
+Explanation
 The initial linked list is: .
 
 The reversed linked list is: .
