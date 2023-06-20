41e6a720b3b1b25e81fae66a6522bbbccd46f8eb
Rohit Sahu <113296241+Rohit-304@users.noreply.github.com>
2023-06-20 13:12:39 +0530

    Update Output_Formating.java

--- JAVA/Output_Formating.java
+++ JAVA/Output_Formating.java
@@ -1,4 +1,5 @@
 /*
+
 Java OutputFormattingJava's System.out.printf function can be used to print formatted output. 
 The purpose of this exercise is totest your understanding of formatting output using printf.
 To get you started, a portion of the solution is provided for you in the editor; you must format and print the input to complete the solution.
