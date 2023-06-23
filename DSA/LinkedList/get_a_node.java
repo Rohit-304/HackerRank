cb96ff9c97c941826ab061f33fcd6c01a29f59fc
Rohit Sahu <113296241+Rohit-304@users.noreply.github.com>
2023-06-23 10:52:27 +0530

    Get Node Value

--- DSA/LinkedList/get_a_node.java
+++ DSA/LinkedList/get_a_node.java
@@ -1,4 +1,5 @@
 /*
+
 This challenge is part of a tutorial track by MyCodeSchool
 
 Given a pointer to the head of a linked list and a specific position, determine the data value at that position. Count backwards from the tail node. The tail is at postion 0, its parent is at 1 and so on.
