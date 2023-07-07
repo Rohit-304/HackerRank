a495551d1c52da63d53fb0a458ef99edb1403e9d
Rohit Sahu <113296241+Rohit-304@users.noreply.github.com>
2023-07-07 09:09:04 +0530

    Find Merge Point of Two Lists

--- DSA/LinkedList/MergePoint_of_two_lists.java
+++ DSA/LinkedList/MergePoint_of_two_lists.java
@@ -1,4 +1,5 @@
 /*
+
 This challenge is part of a tutorial track by MyCodeSchool
 
 Given pointers to the head nodes of  linked lists that merge together at some point, find the node where the two lists merge. The merge point is where both lists point to the same node, i.e. they reference the same memory location. It is guaranteed that the two head nodes will be different, and neither will be NULL. If the lists share a common node, return that node's  value.
