4f159025f4c7272fd052949cc5adaf35e489cbf8
Rohit Sahu <113296241+Rohit-304@users.noreply.github.com>
2023-10-05 23:42:54 +0530

    if-Else

--- JAVA/if-Else.java
+++ JAVA/if-Else.java
@@ -7,7 +7,7 @@ If  is odd, print Weird
 If  is even and in the inclusive range of  to , print Not Weird
 If  is even and in the inclusive range of  to , print Weird 
 If  is even and greater than , print Not Weird
-Complete the stub code provided in your editor to print whether or not  is weird. 
+Complete the stub code provided in your editor to print whether or not  is weird.  
  
 Input Format
 A single line containing a positive integer, .
